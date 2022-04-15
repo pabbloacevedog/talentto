c035ff23ea1b794ca44b2ca0a6830caf0a6bcb08
paceved.11 <pacevedo@aligare.cl>
2022-04-14 22:58:26 -0400

    heroku

--- src/boot/apollo.js
+++ src/boot/apollo.js
@@ -1,29 +1,13 @@
 import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
-import { setContext as ST } from "apollo-link-context";
 import VueApollo from "vue-apollo";
 import fetch from "node-fetch";
 import { createHttpLink } from "apollo-link-http";
 
-const httpLink = createHttpLink({
-	uri: process.env.API_URL,
-	fetch: fetch,
-});
+const httpLink = createHttpLink({ uri: process.env.API_URL, fetch: fetch });
 
-const authLink = ST((_, { headers }) => {
-	// get the authentication token from local storage if it exists
-	const token = localStorage.getItem("token");
-	// return the headers to the context so httpLink can read them
-	return {
-		headers: {
-			...headers,
-			authorization: token ? `Bearer ${token}` : "",
-		},
-	};
-});
-// Create the apollo client
 const apolloClient = new ApolloClient({
-	link: authLink.concat(httpLink),
+	link: httpLink,
 	cache: new InMemoryCache(),
 	connectToDevTools: true,
 });
@@ -44,8 +28,10 @@ const apollo = new VueApollo({
 	},
 });
 
+// window.__APOLLO_CLIENT__ = apollo
 export default ({ app, Vue, store }) => {
-	Vue.use(VueApollo);
+	Vue.use(apollo);
 	app.apolloProvider = apollo;
 	store.$apollo = apollo;
+	Vue.prototype.$apollo = apollo;
 };
