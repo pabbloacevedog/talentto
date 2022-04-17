f8c536bfae021e195e29256705ebcc96f90e49ba
paceved.11 <pacevedo@aligare.cl>
2022-04-16 20:30:16 -0400

    graphql

--- src/boot/apollo.js
+++ src/boot/apollo.js
@@ -1,14 +1,7 @@
 import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
-import { setContext as ST } from "apollo-link-context";
 import VueApollo from "vue-apollo";
 import fetch from "node-fetch";
-// import { createHttpLink } from 'apollo-link-http'
-import { split } from "apollo-link";
-import { createUploadLink } from "apollo-upload-client";
-import { getMainDefinition } from "apollo-utilities";
-
-// const httpLink = createHttpLink({ uri: process.env.API_URL, fetch: fetch })
 const httpLink = createHttpLink({
 	uri: process.env.GRAPHQL_URL,
 	fetch: fetch,
@@ -16,20 +9,6 @@ const httpLink = createHttpLink({
 
 const apolloClient = new ApolloClient({
 	link: httpLink,
-	// link: ApolloLink.from([
-	// 	onError(({ graphQLErrors, networkError }) => {
-	// 		if (graphQLErrors)
-	// 			graphQLErrors.map(({ message, locations, path }) =>
-	// 				console.log(
-	// 					`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
-	// 				)
-	// 			);
-	// 		if (networkError) console.log(`[Network error]: ${networkError}`);
-	// 	}),
-	// 	authLink,
-	// 	// omitTypenameLink,
-	// 	terminatingLink,
-	// ]),
 	cache: new InMemoryCache({ addTypename: false }),
 	connectToDevTools: true,
 });
@@ -54,7 +33,7 @@ console.log(
 );
 // window.__APOLLO_CLIENT__ = apollo
 export default ({ app, Vue, store }) => {
-	Vue.use(apollo);
+	Vue.use(VueApollo);
 	app.apolloProvider = apollo;
 	store.$apollo = apollo;
 	Vue.prototype.$apollo = apollo;
